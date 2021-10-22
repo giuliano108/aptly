095d6db7b2b12054f22593fefdb69cc5e4107452
Giuliano <giuliano@108.bz>
2021-10-22 21:49:14 +0100

    TestByPrefix

--- database/sql/database_test.go
+++ database/sql/database_test.go
@@ -54,6 +54,47 @@ func (s *SQLSuite) TestGetPut(c *C) {
 	c.Assert(result, DeepEquals, value)
 }
 
+func (s *SQLSuite) TestByPrefix(c *C) {
+	c.Check(s.db.FetchByPrefix([]byte{0x80}), DeepEquals, [][]byte{})
+
+	s.db.Put([]byte{0x80, 0x01}, []byte{0x01})
+	s.db.Put([]byte{0x80, 0x03}, []byte{0x03})
+	s.db.Put([]byte{0x80, 0x02}, []byte{0x02})
+	c.Check(s.db.FetchByPrefix([]byte{0x80}), DeepEquals, [][]byte{{0x01}, {0x02}, {0x03}})
+	c.Check(s.db.KeysByPrefix([]byte{0x80}), DeepEquals, [][]byte{{0x80, 0x01}, {0x80, 0x02}, {0x80, 0x03}})
+
+	s.db.Put([]byte{0x90, 0x01}, []byte{0x04})
+	c.Check(s.db.FetchByPrefix([]byte{0x80}), DeepEquals, [][]byte{{0x01}, {0x02}, {0x03}})
+	c.Check(s.db.KeysByPrefix([]byte{0x80}), DeepEquals, [][]byte{{0x80, 0x01}, {0x80, 0x02}, {0x80, 0x03}})
+
+	s.db.Put([]byte{0x00, 0x01}, []byte{0x05})
+	c.Check(s.db.FetchByPrefix([]byte{0x80}), DeepEquals, [][]byte{{0x01}, {0x02}, {0x03}})
+	c.Check(s.db.KeysByPrefix([]byte{0x80}), DeepEquals, [][]byte{{0x80, 0x01}, {0x80, 0x02}, {0x80, 0x03}})
+
+	keys := [][]byte{}
+	values := [][]byte{}
+
+	c.Check(s.db.ProcessByPrefix([]byte{0x80}, func(k, v []byte) error {
+		keys = append(keys, append([]byte(nil), k...))
+		values = append(values, append([]byte(nil), v...))
+		return nil
+	}), IsNil)
+
+	c.Check(values, DeepEquals, [][]byte{{0x01}, {0x02}, {0x03}})
+	c.Check(keys, DeepEquals, [][]byte{{0x80, 0x01}, {0x80, 0x02}, {0x80, 0x03}})
+
+	c.Check(s.db.ProcessByPrefix([]byte{0x80}, func(k, v []byte) error {
+		return database.ErrNotFound
+	}), Equals, database.ErrNotFound)
+
+	c.Check(s.db.ProcessByPrefix([]byte{0xa0}, func(k, v []byte) error {
+		return database.ErrNotFound
+	}), IsNil)
+
+	c.Check(s.db.FetchByPrefix([]byte{0xa0}), DeepEquals, [][]byte{})
+	c.Check(s.db.KeysByPrefix([]byte{0xa0}), DeepEquals, [][]byte{})
+}
+
 func (s *SQLSuite) TestHasPrefix(c *C) {
 	c.Check(s.db.HasPrefix([]byte(nil)), Equals, false)
 	c.Check(s.db.HasPrefix([]byte{0x80}), Equals, false)
@@ -64,6 +105,11 @@ func (s *SQLSuite) TestHasPrefix(c *C) {
 	c.Check(s.db.HasPrefix([]byte{0x80}), Equals, true)
 	c.Check(s.db.HasPrefix([]byte{0x79}), Equals, false)
 }
+
+////
+////
+////
+////
 
 func (s *SQLSuite) TestEscapeLikeWildcardCharacters(c *C) {
 	c.Check(sql.EscapeLikeWildcardCharacters([]byte("foo"), []byte("\\")), DeepEquals, []byte("foo"))
--- database/sql/storage.go
+++ database/sql/storage.go
@@ -8,14 +8,17 @@ import (
 )
 
 type storage struct {
-	driverName      string
-	dataSourceName  string
-	tableName       string
-	escapeCharacter []byte
-	db              *databasesql.DB
-	putStmt         *databasesql.Stmt
-	getStmt         *databasesql.Stmt
-	countPrefixStmt *databasesql.Stmt
+	driverName        string
+	dataSourceName    string
+	tableName         string
+	escapeCharacter   []byte
+	db                *databasesql.DB
+	putStmt           *databasesql.Stmt
+	getStmt           *databasesql.Stmt
+	countPrefixStmt   *databasesql.Stmt
+	fetchPrefixStmt   *databasesql.Stmt
+	keysPrefixStmt    *databasesql.Stmt
+	processPrefixStmt *databasesql.Stmt
 }
 
 func (s *storage) Get(key []byte) ([]byte, error) {
@@ -36,13 +39,33 @@ func (s *storage) Delete(key []byte) error {
 	panic("not implemented") // TODO: Implement
 }
 
+// FetchByPrefix returns all values with keys that start with prefix
+func (s *storage) FetchByPrefix(prefix []byte) [][]byte {
+	values := make([][]byte, 0, 20)
+	rows, err := s.fetchPrefixStmt.Query(PrefixPattern(prefix, s.escapeCharacter))
+	if err != nil {
+		panic("error")
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var value []byte
+		if err := rows.Scan(&value); err != nil {
+			panic("error")
+		}
+		values = append(values, value)
+	}
+	if err = rows.Err(); err != nil {
+		panic("error")
+	}
+	return values
+}
+
 // HasPrefix checks whether it can find any key with given prefix and returns true if one exists
 func (s *storage) HasPrefix(prefix []byte) bool {
 	var count int
 	err := s.countPrefixStmt.QueryRow(PrefixPattern(prefix, s.escapeCharacter)).Scan(&count)
 	if err != nil {
-		// PrefixReader.HasPrefix doesn't want us to do real error handling
-		return false
+		panic("error")
 	}
 	if count > 0 {
 		return true
@@ -51,16 +74,51 @@ func (s *storage) HasPrefix(prefix []byte) bool {
 	}
 }
 
+// ProcessByPrefix iterates through all entries where key starts with prefix and calls
+// StorageProcessor on key value pair
 func (s *storage) ProcessByPrefix(prefix []byte, proc database.StorageProcessor) error {
-	panic("not implemented") // TODO: Implement
+	rows, err := s.processPrefixStmt.Query(PrefixPattern(prefix, s.escapeCharacter))
+	if err != nil {
+		panic("error")
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var key []byte
+		var value []byte
+		if err := rows.Scan(&key, &value); err != nil {
+			panic("error")
+		}
+		err := proc(key, value)
+		if err != nil {
+			return err
+		}
+	}
+	if err = rows.Err(); err != nil {
+		panic("error")
+	}
+
+	return nil
 }
 
+// KeysByPrefix returns all keys that start with prefix
 func (s *storage) KeysByPrefix(prefix []byte) [][]byte {
-	panic("not implemented") // TODO: Implement
-}
-
-func (s *storage) FetchByPrefix(prefix []byte) [][]byte {
-	panic("not implemented") // TODO: Implement
+	keys := make([][]byte, 0, 20)
+	rows, err := s.keysPrefixStmt.Query(PrefixPattern(prefix, s.escapeCharacter))
+	if err != nil {
+		panic("error")
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var key []byte
+		if err := rows.Scan(&key); err != nil {
+			panic("error")
+		}
+		keys = append(keys, key)
+	}
+	if err = rows.Err(); err != nil {
+		panic("error")
+	}
+	return keys
 }
 
 func (s *storage) CreateBatch() database.Batch {
@@ -95,6 +153,18 @@ func (s *storage) Open() error {
 		return err
 	}
 	s.countPrefixStmt, err = s.db.Prepare("SELECT COUNT (key) as count FROM " + s.tableName + " WHERE KEY LIKE ? ESCAPE '" + string(s.escapeCharacter) + "'")
+	if err != nil {
+		return err
+	}
+	s.fetchPrefixStmt, err = s.db.Prepare("SELECT value FROM " + s.tableName + " WHERE KEY LIKE ? ESCAPE '" + string(s.escapeCharacter) + "' ORDER BY key")
+	if err != nil {
+		return err
+	}
+	s.keysPrefixStmt, err = s.db.Prepare("SELECT key FROM " + s.tableName + " WHERE KEY LIKE ? ESCAPE '" + string(s.escapeCharacter) + "' ORDER BY key")
+	if err != nil {
+		return err
+	}
+	s.processPrefixStmt, err = s.db.Prepare("SELECT key, value FROM " + s.tableName + " WHERE KEY LIKE ? ESCAPE '" + string(s.escapeCharacter) + "' ORDER BY key")
 	return err
 }
 
